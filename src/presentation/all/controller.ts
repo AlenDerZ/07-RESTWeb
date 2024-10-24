1e6d17e26a83d33dbb788411818b42b526897368
Alejandra Buentello <alenlbuentello7618@gmail.com>
2024-10-24 12:23:01 -0600

    CRUD - Prisma

--- src/presentation/all/controller.ts
+++ src/presentation/all/controller.ts
@@ -1,73 +1,70 @@
+import { prisma } from "../../data/postgres"; 
 import { Request, Response } from "express";
-
-const all = [
-    {id: 1, name: 'Kuro', completedAt: new Date()},
-    {id: 2, name: 'Xiao', completedAt: null},
-    {id: 3, name: 'Luffy', completedAt: new Date()},
-    {id: 4, name: 'Hua Cheng', completedAt: new Date()},
-    {id: 5, name: 'Xie Lian', completedAt: new Date()},
-    {id: 6, name: 'Nami', completedAt: new Date()},
-]
 
 export class AllController {
 
     //* Dependencies Injection
     constructor() {}
 
-    public getAll = (req:Request, res:Response) => {
-        return res.json(all)
+    public getAll = async (req:Request, res:Response) => {
+        const all = await prisma.all.findMany();
+        res.json(all)
     }
 
-    public getAllById = (req:Request, res:Response) => {
+    public getAllById = async (req:Request, res:Response) => {
         const id = +req.params.id;
         if(isNaN(id)) return res.status(400).json({error: 'Id must be a number'});
-        const result = all.find(item => item.id === id);
+        // const result = all.find(item => item.id === id);
+        const result = await prisma.all.findFirst({where: {id}});
 
         (result)
             ? res.json(result)
             : res.status(404).json({error: `All with id ${id} not found`});
     }
 
-    public createAll = (req:Request, res:Response) => {
+    public createAll = async (req:Request, res:Response) => {
         const {name} = req.body;
         if(!name) return res.status(400).json({error: 'Name is required'});
 
-        const newAll = {
-            id: all.length + 1,
-            name: name,
-            completedAt: null
-        }
-
-        all.push(newAll)
-        res.json(newAll);
+        const all = await prisma.all.create({
+            data: { name }
+        });
+        
+        res.json(all);
     }
 
-    public updateAll = (req:Request, res:Response) => {
+    public updateAll = async (req:Request, res:Response) => {
         const id = +req.params.id;
         if(isNaN(id)) return res.status(400).json({error: 'Id must be a number'});
 
-        const result = all.find(item => item.id === id);
+        const result = await prisma.all.findFirst({where: {id}});
         if(!result) return res.status(404).json({error: `All with id ${id} not found`});
 
         const {name, completedAt} = req.body;
 
-        result.name = name || result.name;
-        (completedAt === null) 
-            ? result.completedAt = null
-            : result.completedAt = new Date(completedAt || result.completedAt);
+        const all = await prisma.all.update({
+            where: {id},
+            data: { 
+                name, 
+                completedAt: (completedAt) ? new Date(completedAt) : null
+            }
+        });
 
-        res.json(result);
+        res.json(all);
     }
 
-    public deleteAll = (req:Request, res:Response) => {
+    public deleteAll = async (req:Request, res:Response) => {
         const id = +req.params.id;
         if(isNaN(id)) return res.status(400).json({error: 'Id must be a number'});
 
-        const result = all.find(item => item.id === id);
+        const result = await prisma.all.findFirst({where: {id}});
         if(!result) return res.status(404).json({error: `All with id ${id} not found`});
 
-        all.splice(all.indexOf(result), 1);
-        res.json(result);
+        const deleted = await prisma.all.delete({where: {id}});
+
+        (deleted)
+            ? res.json({deleted})
+            : res.status(400).json({error: `All with id ${id} not found`});
 
     }
 }
