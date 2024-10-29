020e926936531a0591afa1d59b5a87238d72ce62
Alejandra Buentello <alenlbuentello7618@gmail.com>
2024-10-29 12:53:39 -0600

    Testing - GET - api/all

--- test/presentation/all/routes.test.ts
+++ test/presentation/all/routes.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres';
+import exp from 'constants';
 
 describe('All route testing', () => {
 
@@ -11,13 +13,26 @@ describe('All route testing', () => {
         testServer.close();
     });
 
+    const all1 = {name: 'Hua Cheng'};
+    const all2 = {name: 'Xie Lian'};
+
     test('should return ALLs api/all', async() => {
 
-        const response = await request(testServer.app)
+        await prisma.all.deleteMany();
+        await prisma.all.createMany({
+            data: [all1, all2]
+        });
+
+        const {body} = await request(testServer.app)
             .get('/api/all')
             .expect(200); 
 
-        console.log(response.body);
+        expect(body).toBeInstanceOf(Array);
+        expect(body).toHaveLength(2);
+        expect(body[0].name).toBe(all1.name);
+        expect(body[1].name).toBe(all2.name);
+        expect(body[0].completedAt).toBeUndefined();
+        expect(body[1].completedAt).toBeUndefined();
 
     })
 })
